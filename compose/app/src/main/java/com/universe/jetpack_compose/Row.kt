7f272655ff8d58480f775d7615eafb5edbe10103
fenil09 <backupsystem9909@gmail.com>
2023-03-20 10:02:13 +0530

    Worked with Modifiers and its different attributes and used to create simple layout.

--- compose/app/src/main/java/com/universe/jetpack_compose/Row.kt
+++ compose/app/src/main/java/com/universe/jetpack_compose/Row.kt
@@ -1,12 +1,12 @@
 package com.universe.jetpack_compose
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -15,8 +15,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.universe.jetpack_compose.ui.theme.Jetpack_ComposeTheme
 
+@Suppress("LABEL_RESOLVE_WILL_CHANGE")
 class Row : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +34,19 @@ class Row : ComponentActivity() {
                 Text("Never give up")
                 Text("Life is not fair always")
                 Text("Always keep fighting")
+                Spacer(modifier = Modifier.height(50.dp))
+            }
+
+            Column (modifier = Modifier
+                .background(Color.Black)
+
+            ){
+                Button(onClick = {
+                    val intent: Intent= Intent(this@Row,Modifiers::class.java)
+                    startActivity(intent)
+                }){
+                    Text("Goto new Window")
+                }
             }
         }
     }
